5ca5a5e366325675e8a3ef6f2c4fcf63120e4bde
JamilJalal <jamil.jalal94@gmail.com>
2018-12-27 04:49:30 -0500

    App done, Challenge and Gif Updated

--- goalpost/Controller/GoalsVC.swift
+++ goalpost/Controller/GoalsVC.swift
@@ -16,13 +16,16 @@ class GoalsVC: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
     var goals: [Goal] = []
+    var removedItems: [Goal] = []
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.delegate = self
         tableView.dataSource = self
         tableView.isHidden = false
         // Do any additional setup after loading the view, typically from a nib.
-
+        
         
     }
     
@@ -52,6 +55,11 @@ class GoalsVC: UIViewController {
     }
     
     
+    @IBAction func undoBtn(_ sender: Any) {
+        
+        self.undoItem()
+        self.tableView.reloadData()
+    }
     
 }
 
@@ -86,16 +94,43 @@ extension GoalsVC: UITableViewDelegate, UITableViewDataSource {
             tableView.deleteRows(at: [indexPath], with: .automatic)
         }
         
+        let addAction = UITableViewRowAction(style: .normal, title: "Add 1") { (rowAction, indexPath) in
+            self.setProgress(atIndexPath: indexPath)
+            tableView.reloadRows(at: [indexPath], with: .automatic)
+        }
+        
+      
         deleteAction.backgroundColor = #colorLiteral(red: 0.7450980544, green: 0.1568627506, blue: 0.07450980693, alpha: 1)
-        return [deleteAction]
+        addAction.backgroundColor = #colorLiteral(red: 0.4274509804, green: 0.737254902, blue: 0.3882352941, alpha: 1)
+        
+        return [deleteAction, addAction]
     }
     
-}
+    }
 
 extension GoalsVC {
     
+    func setProgress(atIndexPath indexPath: IndexPath){
+        guard let managedContext = appDelegate?.persistentContainer.viewContext else {return}
+        let chosenGoal = goals[indexPath.row]
+        
+        if chosenGoal.goalProgress < chosenGoal.goalCompletionValue {
+            chosenGoal.goalProgress = chosenGoal.goalProgress + 1
+        }else{
+            return
+        }
+        
+        do{
+            try managedContext.save()
+            print("Set")
+        }catch{
+            debugPrint("Could not set progress: \(error.localizedDescription)")
+        }
+    }
+    
     func removeGoal(atIndexPath indexPath: IndexPath){
         guard let managedContext = appDelegate?.persistentContainer.viewContext else {return}
+        managedContext.undoManager = UndoManager() // We have to set the undo manager, if we dont the managed context doesnt have an UndoManager
         managedContext.delete(goals[indexPath.row])
         do{
             try managedContext.save()
@@ -104,6 +139,13 @@ extension GoalsVC {
             debugPrint("Could not remove \(error.localizedDescription)")
         }
     }
+    
+    func undoItem(){
+        guard let managedContext = appDelegate?.persistentContainer.viewContext else {return}
+        managedContext.undoManager?.undo() // We undo using this function
+        fetchCoreDataObject()
+    }
+    
     func fetch(completion: (_ complete: Bool) ->()){
         guard let managedContext = appDelegate?.persistentContainer.viewContext else {return}
         let fetchRequest = NSFetchRequest<Goal>(entityName: "Goal")
